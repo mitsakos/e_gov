faaf34c6a2c64079bfd939c3a4d7907ede904d31
Apollon <jim_metal@yahoo.com>
2012-08-21 22:46:13 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EGov::Application.config.secret_token = 'b47f57da3c40c9b8868c9a0b0706e257f6b148f6809fe5ab981fdabbf886bbda1d6d6c7696e9120b0aef9966f601bf2b51c4a56dcaf246d6c848672214620917'
